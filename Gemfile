3570fe6ca94ee0fbdaf8724bee8ad1e2c4d15aa8
dzhiqin <dzhiqin@126.com>
2018-02-05 18:12:45 +0800

    设置了中英双语选项

--- Gemfile
+++ Gemfile
@@ -40,7 +40,7 @@ gem 'devise'
 gem 'font-awesome-rails'
 gem 'rails-i18n'
 gem 'devise-i18n'
-
+gem 'pry'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,50 @@
 module ApplicationHelper
   def render_single_or_deplex(house)
     if house.is_duplex
-      '复式'
+      t('is_duplex')
     else
-      '单层'
+      t('is_single')
+    end
+  end
+  def render_yes_or_no(condition)
+    if condition==='1'
+      t('my_yes')
+    else
+      t('my_no')
+    end
+  end
+  def render_with_or_without(condition)
+    if condition === '1'
+      t('my_with')
+    else
+      t('my_without')
+    end
+  end
+  def render_malleability_degree(condition)
+    if condition === '0'
+      t('low')
+    elsif condition ==='1'
+      t('middle')
+    elsif condition ==='2'
+      t('high')
+    end
+  end
+  def render_education_resources_degree(condition)
+    if condition === '0'
+      t('rare')
+    elsif condition === '1'
+      t('medium')
+    elsif condition === '2'
+      t('plenty')
+    end
+  end
+  def render_convinence_level(condition)
+    if condition === '0'
+      t('low')
+    elsif condition === '1'
+      t('middle')
+    elsif condition === '2'
+      t('high')
     end
   end
 end

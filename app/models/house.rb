67a5d6874943245d48cf36bd11b2fb309cd967c7
dzhiqin <dzhiqin@126.com>
2018-02-06 11:06:41 +0800

    拆除turbolinks

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,4 +1,6 @@
 class House < ApplicationRecord
   validates_presence_of :house_id,:contact_phone,:address,:spatial_planning,:house_upgrade,:gas_pipeline,:elevator,:sercurity_service,:charter_costs
-
+  validates_format_of :contact_phone,
+    :with=> /1\d{10}/,
+    :message=> 'must be a phone number!'
 end

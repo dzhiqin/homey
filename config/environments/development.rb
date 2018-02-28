900844ee8b17ba0715c5d695d5804531c196190f
dzhiqin <dzhiqin@126.com>
2018-02-28 22:18:53 +0800

    安装figaro

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,7 +5,18 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
-
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method=:smtp
+  ActionMailer::Base.smtp_settings={
+    address:"smtpcloud.sohu.com",
+    port: 25,
+    domain: "heroku.com",
+    authentication: "login",
+    enable_starttls_auto: true,
+    user_name: ENV["SEND_CLOUD_USER_NAME"],
+    password: ENV["SEND_CLOUD_USER_KEY"]
+    }
   # Do not eager load code on boot.
   config.eager_load = false
 

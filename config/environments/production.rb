095538687d82d2ec01bd981b1c3800ffa64a4260
dzhiqin <dzhiqin@126.com>
2018-03-16 15:51:38 +0800

    调试sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,14 +67,14 @@ Rails.application.configure do
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
-  config.action_mailer.smtp_settings = {
-      :address        => 'smtp.sendgrid.net',
-      :port           => '587',
-      :authentication => :plain,
-      :user_name      => ENV['SENDGRID_USERNAME'],
-      :password       => ENV['SENDGRID_PASSWORD'],
-      :domain         => 'heroku.com',
-      :enable_starttls_auto => true
+  ActionMailer::Base.smtp_settings={
+    address:"smtp.sendgrid.net",
+    port: 587,
+    domain: "heroku.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["SENDGRID_USERNAME"],
+    password: ENV["SENDGRID_PASSWORD"]
     }
 
     config.action_mailer.delivery_method = :smtp

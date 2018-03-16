968b355247b6c9efa26cf0f9fdf4da4c7c9eba71
dzhiqin <dzhiqin@126.com>
2018-03-16 17:33:57 +0800

    add confirmation to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180225094109) do
+ActiveRecord::Schema.define(version: 20180316085716) do
 
   create_table "districts", force: :cascade do |t|
     t.string   "name"
@@ -96,6 +96,11 @@ ActiveRecord::Schema.define(version: 20180225094109) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

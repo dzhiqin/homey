6e8e0016280d27b1294bc1522beae775b41c1f7a
dzhiqin <dzhiqin@126.com>
2018-03-20 11:33:56 +0800

    删除generate_id预赋值

--- db/migrate/20180319094347_fix_house_id_class.rb
+++ db/migrate/20180319094347_fix_house_id_class.rb
@@ -2,7 +2,5 @@ class FixHouseIdClass < ActiveRecord::Migration[5.0]
   def change
     change_column :houses,:house_id,:string
   end
-  House.find_each do |house|
-    house.house_id=generate_id
-  end
+  
 end

15ac69b9428f1acc56edd669f6ef4f479bf97ec7
dzhiqin <dzhiqin@126.com>
2019-03-11 20:27:42 +0800

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
 
     config.action_mailer.delivery_method = :smtp
   # ActionMailer Config
-    config.action_mailer.default_url_options = { :host => "https://homey-deploy-test.herokuapp.com" }
+    config.action_mailer.default_url_options = { :host => "https://homey-res.herokuapp.com" }
     config.action_mailer.perform_deliveries = true
     config.action_mailer.raise_delivery_errors = true
   # Use default logging formatter so that PID and timestamp are not suppressed.
